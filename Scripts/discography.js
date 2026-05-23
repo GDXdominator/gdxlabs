5adda90e44a7d6ad67b572548f6514d40b20f83c
GDXdominator <rwatuzi@gmail.com>
2026-05-23 05:06:15 -0700

    touch-action manipulation on tag buttons, new art, new single added to discography

--- Scripts/discography.js
+++ Scripts/discography.js
@@ -13,7 +13,8 @@ var discography = [
     ["GDX", "An Ethereal Rhythm", "2026/02/28", "Single"],
     ["GDX", "KICK KONG VS. KICKZILLA", "2026/03/08", "Single"],
     ["GDX", "Ube Mamon", "2026/03/14", "Single"],
-    ["GDX", "Snort A Lot More Crack", "2026/04/16", "Single"]
+    ["GDX", "Snort A Lot More Crack", "2026/04/16", "Single"],
+    ["Candle Cat", "The Idol is Here", "2026/05/23", "Single"]
 ];
 var chronology = [
     ["GDX", "Drowning in Reverb", "2024/05/09", "Track"],
@@ -29,6 +30,7 @@ var chronology = [
     ["GDX", "Rush", "2024/12/26", "Track"],
     ["GDX", "Break Butcher", "2024/02/10", "Track"],
     ["GDX", "Ube Mamon", "2025/01/24", "Track"],
+    ["Candle Cat", "The Idol is Here", "2025/07/09", "Track"],
     ["GDX", "KICK KONG VS. KICKZILLA", "2025/07/12", "Track"],
     ["GDX", "An Ethereal Rhythm", "2025/07/21", "Track"],
     ["GDX", "MADSCIENCE", "2026/01/26", "Album"],
